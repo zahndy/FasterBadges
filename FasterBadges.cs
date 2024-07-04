21ba20216242488f288381f08f863a4cfb42013e
zahndy <zahndy@gmail.com>
2024-07-04 21:00:59 +0200

    Inital Commit

---
+++ FasterBadges.cs
@@ -0,0 +1,73 @@
+﻿using HarmonyLib;
+using ResoniteModLoader;
+using FrooxEngine;
+using Elements.Core;
+using SkyFrost.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FrooxEngine.CommonAvatar;
+using System.Runtime.CompilerServices;
+
+namespace FasterBadges
+{
+    public class Patch : ResoniteMod
+    {
+        public override String Name => "FasterBadges";
+        public override String Author => "zahndy";
+        public override String Link => "-";
+        public override String Version => "0.0.1";
+
+        public static ModConfiguration Config;
+
+        [AutoRegisterConfigKey]
+        private static ModConfigurationKey<bool> ENABLED = new ModConfigurationKey<bool>("enabled", "Enabled", () => true);
+        [AutoRegisterConfigKey]
+        private static ModConfigurationKey<string> BadgesVar = new ModConfigurationKey<string>("BadgesVar", "List of badges(csv)", () => "");
+
+        public override void OnEngineInit()
+        {
+            Config = GetConfiguration();
+            Config.Save(true);
+            Harmony harmony = new Harmony("com.zahndy.FasterBadges");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(SpawnHelper))]
+        class SpawnHelper_OnUserSpawn_Patch
+        {
+            [HarmonyPrefix]
+            [HarmonyPatch("Spawn")]
+            static void Postfix(UserRoot userRoot) 
+            {
+                if (Config.GetValue(ENABLED)) 
+                {
+                    if (userRoot.ActiveUser.UserName != null)
+                    {
+                        if (userRoot.ActiveUser.IsLocalUser)
+                        {
+                            String[] Badges = Config.GetValue(BadgesVar).Split(',');
+                            if (Badges.Length > 0)
+                            {
+                                AvatarManager avatarManager = userRoot.Slot.GetComponent<AvatarManager>();
+                                TextureFilterMode textureFilterMode = TextureFilterMode.Bilinear;
+                                BlendMode? blendMode = new BlendMode?();
+                                colorX? tint = new colorX?();
+                                int filterMode = (int)textureFilterMode;
+                                int? maxSize = new int?(128);
+                                foreach (String customBadge in Badges)
+                                {
+                                    Uri url = new Uri(customBadge);
+                                    avatarManager.AddIconBadge(url, "Extra Custom Badge-"+ customBadge.Substring(customBadge.Length-10,5), blendMode, tint, (TextureFilterMode)filterMode, maxSize);
+                                }
+                                avatarManager.UpdateBadges();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# FasterBadges
+
+A [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader) mod for [Resonite](https://resonite.com/) that Injects configurable Badges Into the local user<br>
+Requires ResoniteModSettings in order to set the badges string<br>
+badges string is formatted as a comma separated list of resdb: strings.
+
+## Installation
+1. Install [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader).
+1. Place [FasterBadges.dll](https://github.com/zahndy/FasterBadges/releases/latest/download/FasterBadges.dll) into your `rml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\Resonite\rml_mods` for a default install. You can create it if it's missing, or if you launch the game once with ResoniteModLoader installed it will create the folder for you.
+1. Start the game. If you want to verify that the mod is working you can check your Resonite logs.
